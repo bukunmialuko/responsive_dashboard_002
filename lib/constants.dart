f88b638993b231ff4a9b113f85e08a11f558ca07
bukunmialuko <oluwabukunmi.aluko@gmail.com>
2021-10-01 19:52:05 +0100

    Worked on side menu to an extent

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xFF2697FF);
+const secondaryColor = Color(0xFF2A2D3E);
+const bgColor = Color(0xFFF6F8FB);
+
+const defaultPadding = 16.0;
